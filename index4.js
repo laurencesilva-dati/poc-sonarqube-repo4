445262e3982bf87801a83218cc766f389fa71ca6
Laurence Ottoboni <laurence.silva@dati.com.br>
2025-12-18 09:20:32 -0300

    multiples commits 2025-12-18 09-00

--- index4.js
+++ index4.js
@@ -1,27 +1,22 @@
-// Hardcoded credentials (Security Vulnerability)
-//teste20251218-08-25
+//teste20251218-09-00
 
 const DB_USER = "admin";
 const DB_PASSWORD = "admin123";
 
-// Duplicate function (Code Smell + Duplication)
 function login(user, password) {
   if (password == "123456") {
     console.log("Weak password");
   }
 
-  // Always true condition (Bug)
   if (user = "admin") {
     console.log("Logged as admin");
   }
 
-  // SQL Injection simulation (Security Hotspot)
   const query = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + password + "'";
   console.log(query);
 }
 
-// Exact duplicate (Duplication)
-function loginDuplicate(user, password) {
+function login(user, password) {
   if (password == "123456") {
     console.log("Weak password");
   }
@@ -34,30 +29,25 @@ function loginDuplicate(user, password) {
   console.log(query);
 }
 
-// Use of eval (Critical Security Issue)
 function insecureEval(input) {
   eval(input);
 }
 
-// Infinite loop (Bug)
 function infiniteLoop() {
   while (true) {
     console.log("Running forever");
   }
 }
 
-// Unused variable (Code Smell)
 function unusedStuff() {
   let token = "secret-token";
 }
 
-// Exposed secret + bad crypto practice
 const crypto = require("crypto");
 function weakCrypto(password) {
   return crypto.createHash("md5").update(password).digest("hex");
 }
 
-// Callback hell + no error handling
 function callbackHell(cb) {
   setTimeout(() => {
     setTimeout(() => {
